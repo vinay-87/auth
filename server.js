9c615931e363a42d80978e8897efa6bd46d56fba
vinay-87 <158245507+vinay-87@users.noreply.github.com>
2024-04-06 00:16:02 +0530

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.use(express.static('public'));
-
-app.listen(process.env.PORT || port, () => {
-    console.log(`Server is listening at http://localhost:${port}`);
-});
