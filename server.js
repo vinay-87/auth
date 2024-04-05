303b0badd3ab345502185f471dd029fc1e3aa4e5
vinay-87 <158245507+vinay-87@users.noreply.github.com>
2024-04-06 00:07:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes');
-
 const app = express();
 const port = 3000;
 
-// MongoDB connection
-mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// Routes
-app.use('/auth', authRoutes);
-
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
     console.log(`Server is listening at http://localhost:${port}`);
 });
